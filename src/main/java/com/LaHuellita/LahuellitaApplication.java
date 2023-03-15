8d2364f48cdb387d913d0bd0063fcaa9764fd2d6
Joelmora19 <108998021+Joelmora19@users.noreply.github.com>
2023-03-15 10:20:07 -0600

    Add files via upload

---
+++ src/main/java/com/LaHuellita/LahuellitaApplication.java
@@ -0,0 +1,13 @@
+package com.LaHuellita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LahuellitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LahuellitaApplication.class, args);
+	}
+
+}
