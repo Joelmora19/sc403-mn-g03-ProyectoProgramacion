671e38a0d0f9aca61f7eeace418d415a2d3a6a9e
Joelmora19 <108998021+Joelmora19@users.noreply.github.com>
2023-04-03 23:01:05 -0600

    Index

--- src/main/java/com/LaHuellita/controller/IndexController.java
+++ src/main/java/com/LaHuellita/controller/IndexController.java
@@ -5,17 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+    
     @Autowired
     private ArticuloService articuloService;
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String inicio(Model model) {
-        var articulos = articuloService.getArticulos(true);
-        model.addAttribute("articulos", articulos);
         return "index";
     }
 }
+
+
+
